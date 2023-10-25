bac8c5f050ae7374adf987fd5bab444df66309af
BhavyaR-01 <144333670+BhavyaR-01@users.noreply.github.com>
2023-10-25 23:23:44 +0530

    commit

---
+++ digitalclock/src/digitalclock/Main.java
@@ -0,0 +1,39 @@
+package digitalclock;
+public class Main {
+	
+	 public static void main(String[] args) {
+		    int hour = 0;
+		    int minute = 0;
+		    int second = 0;
+
+		    try {
+		      while (true) {
+		        //clear output screen
+		        System.out.print("\033[H\033[2J");
+		        System.out.flush();
+
+		        System.out.printf("%02d : %02d : %02d ", hour, minute, second);
+
+		        second++;
+
+		        if (second == 60) {
+		          minute += 1;
+		          second = 0;
+		        }
+		        if (minute == 60) {
+		          hour += 1;
+		          minute = 0;
+		        }
+		        if (hour == 24) {
+		          hour = 0;
+		          minute = 0;
+		          second = 0;
+		        }
+
+		        Thread.sleep(1000);
+		      }
+		    } catch (Exception e) {
+		      System.out.println(e);
+		    }
+		  }
+		}
